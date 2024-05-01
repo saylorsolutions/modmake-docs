2a58a340f4f445db7c1041fbc218550fa28cbbd8
Joseph Saylor <doug@saylorsolutions.com>
2024-05-01 03:27:24 -0500

    Adding more details to step, task, and builds

--- cmd/modmake-docs/internal/templates/buildmodel.go
+++ cmd/modmake-docs/internal/templates/buildmodel.go
@@ -1,5 +1,10 @@
 package templates
 
+import _ "embed"
+
+//go:embed buildmodel/composability.md
+var composability []byte
+
 func BuildModelSection(params Params) *Section {
 	return NewSection("build-model", "Build Model",
 		BuildModel(),
@@ -7,7 +12,6 @@ func BuildModelSection(params Params) *Section {
 		AddSubSection("steps", "Steps", BuildModel_Steps()).
 		AddSubSection("tasks", "Tasks", BuildModel_Tasks()).
 		AddSubSection("invocation", "Invocation", BuildModel_Invocation(params)).
-		AddSubSection("composability", "Build Composability", BuildModel_Composability()).
-		AddSubSection("app-build", "AppBuild", BuildModel_AppBuild()).
+		AddSubSection("composability", "Build Composability", Markdown(composability)).
 		AddSubSection("api-patterns", "API Patterns", BuildModel_APIPatterns())
 }
