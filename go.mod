e12af6fcf43c193853e5d9b78c24d75889700c36
Joseph Saylor <doug@saylorsolutions.com>
2024-05-01 13:46:06 -0500

    Adding commit hash output

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.22.1
 
 require (
 	github.com/a-h/templ v0.2.663
-	github.com/saylorsolutions/modmake v0.3.1
+	github.com/saylorsolutions/modmake v0.4.0
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.9.0
 	github.com/yuin/goldmark v1.7.1
@@ -13,6 +13,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.16.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- modmake/build.go
+++ modmake/build.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	. "github.com/saylorsolutions/modmake"
+	"github.com/saylorsolutions/modmake/pkg/git"
+)
+
+const (
+	modmakeVersion = "0.4.0"
 )
 
 func main() {
 	b := NewBuild()
-	b.Test().Does(Go().TestAll().Silent())
+	b.Tools().DependsOnRunner("get-modmake", "", Go().Get("github.com/saylorsolutions/modmake@v"+modmakeVersion))
 	b.Tools().DependsOnRunner("install-templ", "", Go().Install("github.com/a-h/templ/cmd/templ@latest"))
 	b.Generate().DependsOnRunner("run-templ", "", Exec("templ", "generate"))
+	b.Test().Does(Go().TestAll().Silent())
 
 	docs := NewAppBuild("modmake-docs", "./cmd/modmake-docs", "0.1.0")
 	docs.Build(func(gb *GoBuild) {
@@ -16,13 +22,15 @@ func main() {
 	})
 	docs.HostVariant().NoPackage()
 	b.ImportApp(docs)
-	b.Step("modmake-docs:install").DependsOn(b.Test())
+	b.Step("modmake-docs:install").
+		DependsOn(b.Test()).
+		BeforeRun(Print("Executing from commit hash: %s", git.CommitHash()))
 
 	b.AddNewStep("run", "serves the docs locally", Go().Run("./cmd/modmake-docs", "serve",
 		F("--base-path=${MM_BASE_PATH:/modmake}"),
 		F("--latest-go=${MM_LATEST_GO:1.22}"),
 		F("--latest-supported=${MM_LATEST_GO_SUPPORTED:1.20}"),
-		F("--modmake-version=${MODMAKE_VERSION:v0.4.0}"),
+		F("--modmake-version=v"+modmakeVersion),
 	)).DependsOn(b.Generate())
 
 	b.Execute()
